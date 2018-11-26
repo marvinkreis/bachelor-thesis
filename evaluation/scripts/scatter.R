6229485e6ade7d4bac613db4f078766877beb22d
Marvin Kreis <MarvinKreis@web.de>
2018-11-26 16:47:43 +0100

    Update R script for the scatter plots

--- evaluation/scripts/scatter.R
+++ evaluation/scripts/scatter.R
@@ -18,48 +18,21 @@ for (name in names(csvs.test_results)) {
     coverage = unlist(lapply(test_coverage, function(x) 100 * x[1,"percent"]));
     coverage[is.na(coverage)] = 0;
 
-    filtered_ids = projects.filtered;
-    filtered_data = data.frame(points = points[filtered_ids],
-                      passes = passes[filtered_ids],
-                      coverage = coverage[filtered_ids]);
-
-    filter_ids = intersect(projects.filter, projects.intersect);
-    filter_data = data.frame(points = points[filter_ids],
-                      passes = passes[filter_ids],
-                      coverage = coverage[filter_ids]);
+    ids = projects.intersect;
+    data = data.frame(points = points[ids],
+                      passes = passes[ids],
+                      coverage = coverage[ids],
+                      excluded = ! projects.intersect %in% projects.filtered);
 
     print(name);
-    print(filtered_data);
+    print(data);
 
-    scatter = ggplot() +
-        geom_point(size = 2,                 data = filtered_data, aes(x = points, y = passes, color = coverage)) +
-        geom_point(size = 2, shape = 1,      data = filter_data,   aes(x = points, y = passes, color = coverage)) +
-        geom_smooth(method = lm, se = FALSE, data = filtered_data, aes(x = points, y = passes, color = coverage)) +
+    scatter = ggplot(data = data, aes(x = points, y = passes, color = coverage, shape = excluded)) +
+        geom_point(size = 2) +
+        geom_smooth(method = lm, se = FALSE, data = subset(data, !excluded)) +
         scale_color_gradient(low = "red", high = "green") +
-        theme_light() +
-        labs(x = "Points", y = "Test Passes", color = "Coverage");
+        scale_shape_manual(values = c(19, 1)) +
+        labs(x = "Points (Manual Evaluation)", y = "Test Passes", color = "Coverage", shape = "Excluded") +
+        theme_light();
     ggsave(paste("scatter-", name, ".pdf", sep=""), plot = scatter, width = 12.5, height = 10, units = "cm");
 }
-
-
-
-
-
-# for (do_filter in FALSE:TRUE) {
-#     if (do_filter) {
-#         filter = "unfiltered";
-#         ids = projects.intersect;
-#     } else {
-#         filter = "filtered";
-#         ids = projects.filtered;
-#     }
-#
-#     data = data.frame(points = points[ids], passes = passes[ids], coverage = coverage[ids]);
-#     scatter = ggplot(data = data, aes(x = points, y = passes, color = coverage)) +
-#         geom_point(size = 2) +
-#         geom_smooth(method = lm, se = FALSE) +
-#         scale_color_gradient(low = "red", high = "green") +
-#         theme_light() +
-#         labs(x = "Points", y = "Test Passes", color = "Coverage");
-#     ggsave(paste("scatter-", name, "-", filter, ".pdf", sep=""), plot = scatter, width = 12.5, height = 10, units = "cm");
-# }
