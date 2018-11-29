f67f37444ccde75caf231b3bdff12631280572d1
Marvin Kreis <MarvinKreis@web.de>
2018-11-29 18:49:45 +0100

    Add correlation to scatter plots

--- evaluation/scripts/scatter.R
+++ evaluation/scripts/scatter.R
@@ -4,6 +4,9 @@
 # One scatter plot displays the unfiltered data, one displays the filtered data.
 
 library(ggplot2);
+library(grid)
+library(gtable)
+
 source("scripts/read-data.R");
 
 points = csvs.keller_data.points;
@@ -24,6 +27,8 @@ for (name in names(csvs.test_results)) {
                       coverage = coverage[ids],
                       excluded = ! projects.intersect %in% projects.filtered);
 
+    correlation = round(cor(data$points[!data$excluded], data$passes[!data$excluded]), digits = 4);
+
     print(name);
     print(data);
 
@@ -34,5 +39,23 @@ for (name in names(csvs.test_results)) {
         scale_shape_manual(values = c(19, 1)) +
         labs(x = "Points (Manual Evaluation)", y = "Test Passes", color = "Coverage", shape = "Excluded") +
         theme_light();
-    ggsave(paste("scatter-", name, ".pdf", sep=""), plot = scatter, width = 12.5, height = 10, units = "cm");
+
+        label = paste("Correlation:\n", correlation, sep = "");
+
+        # https://stackoverflow.com/questions/32506444/ggplot-function-to-add-text-just-below-legend
+        g = ggplotGrob(scatter);
+        leg = g$grobs[[which(g$layout$name == "guide-box")]];
+        textgrob = textGrob(x = unit(5, "points"), label, gp = gpar(cex = .75), just = "left");
+        width = unit(1, "grobwidth",textgrob) + unit(10, "points");
+        height = unit(1, "grobheight", textgrob)+ unit(10, "points");
+        labelGrob = gTree("labelGrob", children = gList(textgrob));
+        pos = subset(leg$layout, grepl("guides", name), t:r)[2,];
+        leg = gtable_add_rows(leg, height, pos = pos$t+1);
+        leg = gtable_add_grob(leg, labelGrob, t = pos$t+2, l = pos$l);
+        leg$widths[pos$l] = max(width, leg$widths[pos$l]);
+        leg$heights[pos$t+1] = unit(20, "pt");
+        g$grobs[[which(g$layout$name == "guide-box")]] = leg;
+        g$widths[g$layout[grepl("guide-box", g$layout$name), "l"]] = max(width, sum(leg$widths));
+
+        ggsave(paste("scatter-", name, ".pdf", sep=""), plot = g, width = 12.5, height = 10, units = "cm");
 }
