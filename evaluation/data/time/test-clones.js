afcc5555a2f64d990ec6065a085769f1881fd7cc
Marvin Kreis <MarvinKreis@web.de>
2019-01-31 03:21:50 +0100

    Update evaluation data

--- evaluation/data/time/test-clones.js
+++
@@ -1,8 +0,0 @@
-const test = async function (t) {
-    await t.runForTime(10000);
-    t.end();
-}
-
-module.exports = [
-    test
-];
--- evaluation/data/time/test-sprites.js
+++
@@ -1,8 +0,0 @@
-const test = async function (t) {
-    await t.runForTime(20000);
-    t.end();
-}
-
-module.exports = [
-    test
-];
